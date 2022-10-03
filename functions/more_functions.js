7a7df23b7cb5f52c981da2b61488700dbf5d0146
Bryan <bmiller@tb.yearup.org>
2022-10-03 12:15:02 -0400

    EXERCISE 1 - SLIDE 16 / more_functions.js

---
+++ functions/more_functions.js
@@ -0,0 +1,62 @@
+// EXERCISE 1 SLIDE 16
+function displayMailingLabel(name, address, city,  state, zip){
+    console.log(name); console.log(address); console.log(city); console.log(state);  console.log(zip);
+}
+
+
+
+
+//PART 2
+function addNumbers(num1, num2){
+    let result = Number(num1) + Number(num2);
+    let eq = num1 + " + " + num2 + " = " + result;
+    console.log(eq);
+};
+
+
+//PART 3
+function displayReceipt(totalDue, amountPaid){
+    
+    let changeDue = (Math.abs(totalDue - amountPaid).toFixed(2));
+    let remainingFunds = ((totalDue - amountPaid).toFixed(2));
+    //return "total: " + totalDue;
+    //return "Paid: " + amountPaid; //RETURN ENDS CODE
+    //return "Change: " + changeDue;
+    console.log("Total: $" + totalDue);
+    console.log("Paid: $" + amountPaid);
+    if(amountPaid >= totalDue){
+        console.log("Change: $" + changeDue);
+    }
+    else{
+        console.log("Insufficient Funds \nRequired: $" + remainingFunds);
+    }
+}
+
+//let receiptCall = displayReceipt(20, 20.53);
+
+
+//CALLING ALL THE FUNCTIONS
+displayMailingLabel("Bryan", "3333 aha rd", "My City", "FL", "336339");
+
+console.log(    );
+
+addNumbers(5, 19);
+
+/*
+let someNum1 = 5;
+let someNum2 = 19;
+
+addNumbers(someNum1, someNum2);
+
+someNum1 = 3;
+someNum2 = 6;
+
+addNumbers(someNum1, someNum2);
+*/
+console.log(     );
+
+displayReceipt(20,39.87);
+console.log( );
+displayReceipt(20, 20);
+console.log( );
+displayReceipt(20, 3.36);
--- functions/simple_functions.js
+++ functions/simple_functions.js
@@ -1,3 +1,4 @@
+//  WORKBOOK 3 EXERCISE 1 - SLIDE 10
 function favoriteThings(){
     console.log("Bryan");
     console.log("I Am Legend - Will Smith");
@@ -5,11 +6,11 @@ function favoriteThings(){
 }
 
 function whyImHere(){
-    console.log("I have a passion for computers and wanted to learn as much as I could from a professional to see how that would be vs being self-taught");
+    console.log("I have a passion for computers and wanted to learn as much as I could from a professional to see how that would be vs being self-taught.");
 }
 
 function favoritePlace(){
-    console.log("My favorite place to visit as of now is my girlfriends house because her make makes some good behind Spanish food");
+    console.log("My favorite place to visit as of now is my girlfriends house because her make makes some good behind Spanish food.");
 }
 
 favoriteThings();
