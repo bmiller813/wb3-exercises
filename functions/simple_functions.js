f847ecc3e8eb2b656396b0bf14fb6a9ba658adab
Bryan <bmiller@tb.yearup.org>
2022-10-03 10:18:08 -0400

    EXERCISE 1 - SLIDE 10

---
+++ functions/simple_functions.js
@@ -0,0 +1,17 @@
+function favoriteThings(){
+    console.log("Bryan");
+    console.log("I Am Legend - Will Smith");
+    console.log("JID");
+}
+
+function whyImHere(){
+    console.log("I have a passion for computers and wanted to learn as much as I could from a professional to see how that would be vs being self-taught");
+}
+
+function favoritePlace(){
+    console.log("My favorite place to visit as of now is my girlfriends house because her make makes some good behind Spanish food");
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
